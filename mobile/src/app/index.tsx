e85662da9687c67c2140460d972abdb767565e82
Monisha <monisha.singh06@gmail.com>
2026-08-23 22:49:18 +0530

    feat: audio recording feature implimented in the expo app

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,31 @@
 import { Text, View, StyleSheet } from "react-native";
+import { useAudioRecorder, RecordingPresets, requestRecordingPermissionsAsync } from 'expo-audio';
+import { Button } from "react-native";
 
 export default function Index() {
+  const recorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
+
+  const startRecording = async () => {
+    const { granted } = await requestRecordingPermissionsAsync();
+
+    if (!granted) {
+      console.log("Microphone permission denied");
+      return;
+    }
+
+    await recorder.prepareToRecordAsync();
+    recorder.record();
+  };
+
+  const stopRecording = async () => {
+    await recorder.stop();
+    console.log("Recording:", recorder.uri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+        <Button title="Start Recording" onPress={startRecording} />
+        <Button title="Stop Recording" onPress={stopRecording} />
     </View>
   );
 }
